3d9ee7dcce3db1dc43197d10b6833d8258a3259c
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-02-23 14:04:11 +0200

    custom list created

---
+++ MyList.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Module3HW1
+{
+    public class MyList<T> :IList<T>, IEnumerable<T> where T : IComparable<T>
+    {
+        public T[] list;
+
+        public const int lenght = 10;
+        private int currentLenght;
+        private int lastIndex;
+        public int CurrentLenght
+        {
+            get { return currentLenght; }
+        }
+        public int LastIndex
+        {
+            get
+            { return lastIndex; }
+        }
+
+        public MyList()
+        {
+            currentLenght = lenght;
+            list = new T[currentLenght];
+            lastIndex = 0;
+        }
+        public MyList(T[] items)
+        {
+            currentLenght = items.Length;
+            lastIndex = items.Length;
+            list = items;
+        }
+
+        public void Add(T item)
+        {
+            if (ListIsFull())
+            {
+                currentLenght = currentLenght * 2;
+
+                T[] newList = new T[currentLenght];
+
+                for (int index = 0; index < list.Length; index++)
+                {
+                    newList[index] = list[index];
+                }
+
+                list = newList;
+            }
+            list[lastIndex++] = item;
+        }
+        public void AddRange(params T[] items)
+        {
+            foreach (var item in items)
+            {
+                if (ListIsFull())
+                    Resize();
+                Add(item);
+            }
+            
+        }
+        private void Resize()
+        {
+            int newLenght = list.Length + list.Length;
+            T[] newList = new T[newLenght];
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                newList[i] = list[i];
+            }
+            currentLenght = newList.Length;
+            list = newList;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                yield return list[i];
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            bool removed = false;
+            T[] newList = new T[currentLenght];
+            for (int i = 0, j = 0; i < currentLenght; i++)
+            {
+                if (list[i].Equals(item))
+                {
+                    removed = true;
+                    lastIndex--;
+                    continue;
+                }
+                newList[j] = list[i];
+                j++;
+            }
+            list = newList;
+
+            return removed;
+        }
+        public void RemoveAt(int index)
+        {
+            T[] newList = new T[currentLenght];
+            index--; /////////////////////////////////
+            for (int i = 0, j = 0; i < currentLenght; i++)
+            {
+                if (i.Equals(index))
+                {
+                    lastIndex--;
+                    continue;
+                }
+                newList[j] = list[i];
+                j++;
+            }
+            list = newList;
+        }
+        public void Sort()
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                for (int j = 0; j < list.Length - 1; j++)
+                {
+                    if (list[j].CompareTo(list[j + 1]) > 0)
+                    {
+                        T item = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = item;
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private bool ListIsFull()
+        {
+            if (lastIndex == list.Length)
+                return true;
+            return false;
+        }
+    
+    }
+}
---
+++ Starter.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3HW1
+{
+    public static class Starter
+    {
+        public static void Run()
+        {
+            StringList();
+            IntList();
+        }
+
+        private static void IntList()
+        {
+            MyList<int> myList = new MyList<int>();
+            Console.WriteLine("\n- Add items to empty list:");
+            myList.Add(5);
+            myList.Add(4);
+            myList.Add(7);
+            myList.Add(9);
+            myList.Add(13);
+            myList.Add(12);
+            myList.Add(1);
+            Print(myList);
+
+            int[] arr = { 12, 47, 9, 101, 21 };
+            Console.Write($"\n- AddRange: {{ 12, 47, 9, 101, 21 }}");
+            Console.WriteLine();
+            myList.AddRange(arr);
+            Print(myList);
+
+            var value = 47;
+            Console.WriteLine($"\n- Remove {{{value}}}:");
+            myList.Remove(value);
+            Print(myList);
+
+            var index = 2;
+            Console.WriteLine($"\n- RemoveAt index [{index}]:");
+            myList.RemoveAt(index);
+            Print(myList);
+
+            Console.WriteLine("\n- Sort int list:");
+            myList.Sort();
+            Print(myList);
+        }
+
+        private static void StringList()
+        {
+            MyList<string> stringList = new MyList<string>(new string[4] { "apple", "snickers", "tea", "car" });
+            Console.WriteLine("- String List:");
+            Print(stringList);
+
+            stringList.Sort();
+            Console.WriteLine("\n- Sort string list:");
+            Print(stringList);
+
+            Console.WriteLine("\n- Add item \"4ur4hella\":");
+            stringList.Add("4ur4hella");
+            Print(stringList);
+        }
+        private static void Print(MyList<int> list)
+        {
+            foreach (var item in list)
+            {
+                if (item == 0)
+                {
+                    continue;
+                }
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine($"\nLast Index: {list.LastIndex} Lenght: {list.CurrentLenght}");
+        }
+
+        private static void Print(MyList<string> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine($"\nLast Index: {list.LastIndex} Lenght: {list.CurrentLenght}");
+        }
+    }
+}
+
